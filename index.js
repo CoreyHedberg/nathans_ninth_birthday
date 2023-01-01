564a644af69addf64209380aa0a01accf59ea0fc
Corey Hedberg <corey@coreyhedberg.dev>
2023-01-01 00:16:00 -0500

    Worked on cleaning up the JS code

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // TODO: Clean up countdown code
-// use strict;
-// Set the date we're counting down to
-const BIRTHDAY_DATE = new Date("Dec 15, 2023 12:00:00").getTime()
 
-// Update the count down every 1 second
-var beginCountdown = setInterval(() => {
+function countdownToBirthday() {
   // Get today's date and time
   const NOW = new Date().getTime()
-
+  // Set the date we're counting down to
+  const BIRTHDAY_DATE = new Date("Dec 15, 2023 24:00:00").getTime()
   // Find the distance between now and the count down date
   const TIME_LEFT = BIRTHDAY_DATE - NOW
-
+  // Addressing what to do when the countdown reaches 0
+  if (TIME_LEFT < 0) {
+    clearInterval(countdownToBirthday)
+    document.getElementById("testing").innerHTML = `It's party time`
+  }
   // Time calculations for days, hours, minutes and seconds
   const DAYS = Math.floor(TIME_LEFT / (1000 * 60 * 60 * 24))
   const HOURS = Math.floor(
@@ -18,15 +19,14 @@ var beginCountdown = setInterval(() => {
   )
   const MINUTES = Math.floor((TIME_LEFT % (1000 * 60 * 60)) / (1000 * 60))
   const SECONDS = Math.floor((TIME_LEFT % (1000 * 60)) / 1000)
+  // Grab the HTML elements and insert the values
+  document.getElementById("days").innerHTML = DAYS
+  document.getElementById("hours").innerHTML = HOURS
+  document.getElementById("minutes").innerHTML = MINUTES
+  document.getElementById("seconds").innerHTML = SECONDS
+}
 
-  // Display the result in the element with id="demo"
-  document.getElementById(
-    "countdown-timer"
-  ).innerHTML = `${DAYS} days ${HOURS} hours ${MINUTES} minutes ${SECONDS} seconds`
-
-  // If the count down is finished, write some text
-  if (TIME_LEFT < 0) {
-    clearInterval(beginCountdown)
-    document.getElementById("countdown-timer").innerHTML = `It's party time`
-  }
-}, 1000)
+// Initialize the function
+countdownToBirthday()
+// Set the count down function to run every 1 second
+setInterval(countdownToBirthday, 1000)
